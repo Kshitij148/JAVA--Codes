22e81f9184356c5a917aea8dc23b4524324e0a05
Kshitij148 <raokshitij14@gmail.com>
2022-01-18 21:57:54 +0530

    Strings in java

---
+++ Strings.java
@@ -0,0 +1,23 @@
+public class Strings {
+	
+	public static void main(String[] args) {
+			
+				int aNumber=9;
+				double aDouble=9.5;
+				String text = "Hello";  // String is a class 
+				String blank=" ";
+				String moreText= "there";
+				String hiThere= text+blank+moreText;
+
+
+				System.out.println(text);
+				System.out.println(text+blank+moreText);
+				System.out.println("Hello"+" "+"there");
+				System.out.println(hiThere);
+
+				System.out.println("My Numbers: "+aNumber+" , "+aDouble);
+
+		}
+}
+
+
