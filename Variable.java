0f75f5f3618be2dc6bef5a02b5b180bdf281b983
Kshitij148 <raokshitij14@gmail.com>
2022-01-18 21:47:15 +0530

    variables in java

---
+++ Variable.java
@@ -0,0 +1,37 @@
+public class Variable {
+	
+	public static void main(String[] args) {
+			
+			int aValue;  // 32 bit
+ 
+			aValue=42;   
+
+			short aShort = 93;  //16 bit
+
+			long aLong = 8304;  //64 bit
+
+			double aDouble = 3.4553;
+
+			float aFloat= 5.234f;
+
+
+			char aChar = 'b';
+
+			boolean aBoolean =true;
+
+			byte aByte=  127;           //holds 6 bit of data
+
+
+
+			System.out.println(aValue);
+			System.out.println(aShort);
+			System.out.println(aLong);
+			System.out.println(aDouble);
+			System.out.println(aFloat);
+			System.out.println(aChar);
+			System.out.println(aBoolean);
+			System.out.println(aByte);
+		}
+}
+
+
