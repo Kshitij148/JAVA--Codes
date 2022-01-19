236e41358e1d2df14210e0f531dc2bce3cc48880
Kshitij148 <raokshitij14@gmail.com>
2022-01-19 17:03:04 +0530

    while loop in java

---
+++ Loops.java
@@ -0,0 +1,25 @@
+public class Loops {
+	
+	public static void main(String[] args) {
+			
+			boolean myLoop=true;
+
+			boolean test= 24<38;
+
+			int number=23;
+
+			System.out.println(myLoop);
+			System.out.println(test);
+
+			while(number < 34){
+
+				System.out.println("Okay");
+				number=number+2;
+			}
+
+		}
+
+
+}
+
+
