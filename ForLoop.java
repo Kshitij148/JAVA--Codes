ac04fef4d68f6f79e7b35f6ae0c715d3883e5e3e
Kshitij148 <raokshitij14@gmail.com>
2022-01-19 17:12:57 +0530

    For loop in java

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+	
+	public static void main(String[] args) {
+
+		//println is short for "print line", meaning after the argument is printed then goes to the next line. printf is short for print formatter, it gives you the ability to mark where in the String variables will go and pass in those variables with it. This saves from having to do a long String concatenation.
+			
+			for(int i=0;i<10;i++){
+
+				System.out.println("I Love dogs");
+				System.out.printf("The value of my variable is: %d\n",i);
+				
+			}
+		}
+}
+
+
