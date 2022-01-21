e28fa5074b1e7334b6c1c2562e6ef7663447911b
Kshitij148 <raokshitij14@gmail.com>
2022-01-21 16:17:14 +0530

    Arrays in java

---
+++ Array.java
@@ -0,0 +1,30 @@
+public class Array {
+	
+	public static void main(String[] args) {
+			
+			int number=10;
+			//int[] numbers = new int[5];
+			int[] numbers={10,20,25,30,35};
+
+			System.out.println(numbers[0]);  //default value in array is 0
+
+			//default value for string in array is null and boolean is false
+
+			numbers[0]=15;
+
+
+
+			System.out.println(numbers[0]);
+
+			for(int i=0;i<numbers.length;i++){
+
+
+				System.out.println(numbers[i]);
+			}
+
+
+		}
+}
+
+
+
